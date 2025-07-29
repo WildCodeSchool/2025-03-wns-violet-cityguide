e953bb313504716976fc79a4ea1b34511cce6663
Laura Buil <128154524+LauraBuil@users.noreply.github.com>
2025-07-29 16:48:45 +0200

    develop (#3)
    * develop
    
    * les trucs qui manquent

---
+++ backend/src/config/db.ts
@@ -0,0 +1,20 @@
+import * as dotenv from "dotenv";
+import { DataSource } from "typeorm";
+import User from "../entities/User";
+
+dotenv.config();
+const { DB_HOST, DB_USER, DB_DATABASE, DB_PASSWORD } = process.env;
+
+const dataSource = new DataSource({
+    type: "postgres",
+    host: DB_HOST,
+    username: DB_USER,
+    password: DB_PASSWORD,
+    database: DB_DATABASE,
+
+    entities: [User],
+    synchronize: true,
+    logging: ["error", "query"],
+});
+
+export default dataSource;
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,
+    allowedHosts: true,
+    hmr: {
+      port: 7000,
+      path: "/hmr",
+    },
+  },
 })
