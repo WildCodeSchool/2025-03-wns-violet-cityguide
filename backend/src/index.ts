f768b9a313a75d59a2a54dcefd1f8c68771c8637
Yhumi0210 <buillaura0210@gmail.com>
2025-07-30 11:33:08 +0200

    feat/ ajout du context et jwt token

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,6 +4,8 @@ import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 import { buildSchema } from "type-graphql";
 import UserResolver from "./resolvers/UserResolver";
+import * as jwt from "jsonwebtoken";
+import {UserToken} from "./types/Context";
 const port = 3000;
 
 async function startServer() {
@@ -14,6 +16,19 @@ async function startServer() {
     const apolloServer = new ApolloServer({ schema });
     const { url } = await startStandaloneServer(apolloServer, {
         listen: { port },
+        context: async ({ req, res }) => {
+            let user: string | jwt.JwtPayload | null = null;
+            const match = req.headers.cookie?.match(/cityGuide-auth=([^;]+)/);
+
+            if (match && process.env.JWT_SECRET) {
+                const token = match[1]
+                // verify renvoie une string si invalide, un payload si valide
+                user = jwt.verify(token, process.env.JWT_SECRET);
+                if (typeof user === "string") user = null;
+            }
+
+            return { req, res, user: user as UserToken };
+        },
     });
     console.info("Server started on " + url);
 }
