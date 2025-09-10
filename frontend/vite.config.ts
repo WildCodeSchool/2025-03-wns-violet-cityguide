853c5ee129c9f7f06e16309ada1361783071a920
Yhumi0210 <buillaura0210@gmail.com>
2025-09-10 12:17:36 +0200

    test (display header): teste l'affichage du header

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,4 +1,5 @@
-import { defineConfig } from 'vite'
+// Config
+import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
@@ -11,5 +12,9 @@ export default defineConfig({
       port: 7000,
       path: "/hmr",
     },
+  },
+  test: {
+    globals: true,
+    environment: 'node',
   },
 })
