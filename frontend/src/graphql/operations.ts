086bf04b4a8469c1aa027c40bc3d1aa0ce433e6e
Rémy Ansar <remy.ansar@gmail.com>
2025-09-18 16:44:24 +0200

    fix(graphql)operation.ts a été modifié pour tenir compte de la maj des entités

--- frontend/src/graphql/operations.ts
+++ frontend/src/graphql/operations.ts
@@ -1,18 +1,18 @@
 import {gql} from "@apollo/client";
 
 export const GET_ALL_USERS = gql`
-    query GetAllUsers {
-        getAllUsers {
-            userId
-            email
-            roles
-            userInfo { 
-                firstName
-                lastName
-                avatarUrl
-            }
-        }
+query GetAllUsers {
+  getAllUsers {
+    userId
+    email
+    roles
+    userInfos {
+      firstName
+      lastName
+      avatarUrl
     }
+  }
+}
 `;
 
 export const SIGNUP = gql`
