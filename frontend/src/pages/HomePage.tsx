7a8b86c26ae0f42d50392249ea68850c69ba4a3c
Yhumi0210 <buillaura0210@gmail.com>
2025-12-10 10:11:38 +0100

    feat (Carousel): ajout du composant Carousel ajoute le composant réutilisable carousel remplace le .map des CityCard pour utiliser le nouveau composant

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,14 +1,33 @@
 // React
 import { useEffect } from "react";
 
+// React UseHook
+import { useWindowSize } from "@uidotdev/usehooks";
+
 // Components
 import CityCard from "../components/CityCard.tsx";
+import Carousel from "../components/Carousel.tsx";
 
 // GraphQL
 import { useGetAllCitiesQuery } from "../generated/graphql-types";
 
+// Types
+import type { City } from "../types/CityType";
+
+function Browser({ size }) {
+	return (
+		<div
+			data-testid="browser"
+			className="browser"
+			style={{ width: size.width / 4, height: size.height / 4 }}
+		/>
+	);
+}
+
 export default function HomePage() {
 	const { data, loading, error } = useGetAllCitiesQuery();
+
+	const size = useWindowSize();
 
 	useEffect(() => {
 		const target = sessionStorage.getItem("scrollTo");
@@ -28,13 +47,21 @@ export default function HomePage() {
 	if (loading) return <p>Loading...</p>;
 	if (error) return <p>Error :(</p>;
 
+	const citiesCards: Array<City> = data?.getAllCities ?? [];
+
 	return (
 		<>
 			<h2 className="heroBanner">Découvrez les points d'intérêts de votre ville</h2>
 			<section className="cityCards" id="cities">
-				{data?.getAllCities.map((currCity) => (
-					<CityCard key={currCity.cityId} city={currCity} />
-				))}
+				{citiesCards.length === 0 ? (
+					<p>Aucune ville trouvée</p>
+				) : (
+					<Carousel visibleCount={4}>
+						{citiesCards.map((currCity) => (
+							<CityCard key={currCity.cityId} city={currCity} />
+						))}
+					</Carousel>
+				)}
 			</section>
 		</>
 	)
