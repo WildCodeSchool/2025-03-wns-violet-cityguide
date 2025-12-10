29d48822fbd023078281e5ec32acf8f46087c0f9
Yhumi0210 <buillaura0210@gmail.com>
2025-12-10 10:52:55 +0100

    feat (Carousel): ajout du composant Carousel ajoute le composant réutilisable carousel remplace le .map des CityCard pour utiliser le nouveau composant

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -3,6 +3,7 @@ import { useEffect } from "react";
 
 // Components
 import CityCard from "../components/CityCard.tsx";
+import Carousel from "../components/Carousel";
 
 // GraphQL
 import { useGetAllCitiesQuery } from "../generated/graphql-types";
@@ -28,13 +29,21 @@ export default function HomePage() {
 	if (loading) return <p>Loading...</p>;
 	if (error) return <p>Error :(</p>;
 
+	const citiesCards = data?.getAllCities ?? [];
+
 	return (
 		<>
 			<h2 className="heroBanner">Découvrez les points d'intérêts de votre ville</h2>
 			<section className="cityCards" id="cities">
-				{data?.getAllCities.map((currCity) => (
-					<CityCard key={currCity.cityId} city={currCity} />
-				))}
+				{citiesCards.length === 0 ? (
+					<p>Aucune ville trouvée</p>
+				) : (
+					<Carousel visibleCount={4}>
+						{citiesCards.map((currCity) => (
+							<CityCard key={currCity.cityId} city={currCity}/>
+						))}
+					</Carousel>
+				)}
 			</section>
 		</>
 	)
