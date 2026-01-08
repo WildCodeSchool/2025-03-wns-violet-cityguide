aa4d2d40aedc09b4f50dec9eda3452c68a1911d1
Yhumi0210 <buillaura0210@gmail.com>
2026-01-08 10:15:02 +0100

    fix (homePage & responsive): ajout de la barre de recherche sur la homePage en mobile

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,61 +1,70 @@
 // React
 import { useEffect } from "react";
+import {  useNavigate } from "react-router-dom";
 
 // Components
 import CityCard from "../components/CityCard.tsx";
 import Carousel from "../components/Carousel";
-// import SearchBar from "../components/SearchBar.tsx";
+import SearchBar from "../components/SearchBar.tsx";
 
 // GraphQL
-import { useGetAllCitiesQuery } from "../generated/graphql-types";
-
-
+import {useGetAllCitiesQuery} from "../generated/graphql-types";
+import type {CityType} from "./City.tsx";
 
 export default function HomePage() {
-	const { data, loading, error } = useGetAllCitiesQuery();
+    const {data, loading, error} = useGetAllCitiesQuery();
+	const navigate = useNavigate();
 
-	const windowSize = window.innerWidth;
+    const windowSize = window.innerWidth;
 
-	useEffect(() => {
-		const target = sessionStorage.getItem("scrollTo");
+    useEffect(() => {
+        const target = sessionStorage.getItem("scrollTo");
 
-		if (target) {
-			sessionStorage.removeItem("scrollTo");
-			const el = document.getElementById(target);
+        if (target) {
+            sessionStorage.removeItem("scrollTo");
+            const el = document.getElementById(target);
 
-			if (el) {
-				setTimeout(() => {
-					el.scrollIntoView({ behavior: "smooth" });
-				}, 0);
-			}
-		}
-	}, []);
+            if (el) {
+                setTimeout(() => {
+                    el.scrollIntoView({behavior: "smooth"});
+                }, 0);
+            }
+        }
+    }, []);
 
-	if (loading) return <p>Loading...</p>;
-	if (error) return <p>Error :(</p>;
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>Error :(</p>;
 
-	const citiesCards = data?.getAllCities ?? [];
+    const citiesCards = data?.getAllCities ?? [];
 
-	return (
-		<>
-			<h2 className="heroBanner">Découvrez les points d'intérêts de votre ville</h2>
-			<section id="cities">
-				{/*<SearchBar cities={cities} currentCity={currentCity} onSelectCity={handleSelectCity} errorMessage={"Aucune ville trouvée"} />*/}
-				{windowSize < 768 ? (
-					<section className="cities-mobile">
-						{citiesCards.map((currCity) => (
-							<CityCard key={currCity.cityId} city={currCity}/>
-						))}
-					</section>
+	const handleSelectCity = (city: CityType) => {
+		navigate(`/city/${city.cityId}`);
+	};
 
-				) : (
-					<Carousel visibleCount={4}>
-						{citiesCards.map((currCity) => (
-							<CityCard key={currCity.cityId} city={currCity}/>
-						))}
-					</Carousel>
-				)}
-			</section>
-		</>
-	)
+    return (
+        <>
+            <h2 className="heroBanner">Découvrez les points d'intérêts de votre ville</h2>
+            <section id="cities">
+                {windowSize < 768 ? (
+                    <section className="cities-mobile">
+                        <SearchBar
+                            cities={citiesCards}
+                            onSelectCity={handleSelectCity}
+                            errorMessage={"Aucune ville trouvée"}
+                        />
+                        {citiesCards.map((currCity) => (
+                            <CityCard key={currCity.cityId} city={currCity}/>
+                        ))}
+                    </section>
+
+                ) : (
+                    <Carousel visibleCount={4}>
+                        {citiesCards.map((currCity) => (
+                            <CityCard key={currCity.cityId} city={currCity}/>
+                        ))}
+                    </Carousel>
+                )}
+            </section>
+        </>
+    )
 }
