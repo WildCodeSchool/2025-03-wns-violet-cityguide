b3b768e09239ba9534dcfc42105bad3fa6cdd20c
Roxane <160009049+dontBelieveIt@users.noreply.github.com>
2026-02-06 17:15:56 +0100

    test(backoffice catégorie) : ajout du script de test de la partie catégorie du backoffice

---
+++ frontend/src/components/tests/BackofficeCategory.test.tsx
@@ -0,0 +1,33 @@
+/**
+ * Ce qu'il faudrait tester : 
+ * - Changement de tab au clic sur les onglets 
+ * - 
+ * Dans la partie "Ajouter une catégorie : "
+ * 	 Voir : nom de la catégorie à ajouter 
+ * 	input qui fonctionne et qui capture les valeurs 
+ * Voir Sélectionner une couleur de pin 
+ * 
+ * importer le hook de couleur de pin => 
+ * 
+ * Tester le bouton submit 
+ * 
+ * Dans administrer une catégorie
+ * => faire des mock categories 
+ * è si pas de catégorie )> le message d'erreur apparait 
+ * sinon tags apparaissent 
+ * -> checker le nombre de tags 
+ * -> checker le nom des Catégories
+ * -> chercker les couleurs de catégories 
+ * 
+ * PUIS modifier une catégorie :  
+ * Au click => on voit la zone d'édition d'une catégorie 
+ * On a un formulaire => tester les champs 
+ * on a un bouton de submit -> tester le boutons de submit 
+ * 
+ * on a une zone de danger => 
+ * le text s'affiche-t-il ? 
+ * au click du bouton => 
+ * Alerte est-elle bien déclenchée ? 
+ * Bouton => annuler la suppression à tester 
+ * bouton => confirmer la suppression à tester 
+ */
