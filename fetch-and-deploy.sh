0a7299106bf77431226882406a0622a8221da4aa
Roxane <160009049+dontBelieveIt@users.noreply.github.com>
2026-03-07 18:04:39 +0100

    NE JAMAIS MERGED ! (versionning) : récupère les fichiers distant de la branche dev-scripts de notre serveur distant - supprime tous les autres dossiers pour autoriser un git clone avec le versionning de cette branche

--- fetch-and-deploy.sh
+++ fetch-and-deploy.sh
@@ -1,11 +1,11 @@
-#!/bin/sh
+#! /bin/sh
 # fetch-and-deploy.sh
+echo fetch-and-deploy.sh
+cd /home/wns_student/apps/dev
+git pull origin develop
 
 # éteint les containers dockers qui tournent
 docker compose -f compose.yaml --env-file .env down && \
 
-    # Pull les nouvelles images
-    docker compose -f compose.yaml --env-file .env pull && \
-
-    # Monte les containers avec les nouvelles images
-    docker compose -f compose.yaml --env-file .env up -d --build;
+  # monte les containers avec les nouvelles images
+  GATEWAY_PORT=7000 docker compose -f compose.yaml --env-file .env up -d --build;
